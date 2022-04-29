5eb46295aecef970e42212c6b2f336f49d155cce
Nishanth80 <90023265+Nishanth80@users.noreply.github.com>
2022-04-29 11:19:01 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,6 +2,6 @@ provider "aws" {
 
   
   region     = "us-west-1"
-  access_key = "AKIAXGL7F2KIMJTHZ6EP"
-  secret_key = "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"
+  access_key = "var.accesskey"
+  secret_key = "var.secretkey"
 }
